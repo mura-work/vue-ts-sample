1836f602e8ffec608b83024e4a0c0ddaab67404b
murata <murata.small.baseball@gmail.com>
2021-07-31 13:57:02 +0900

    create reset-button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <p>{{greetText}}</p>
-    <my-button @click="onMyButtonClicked" :greet="greetText" />
+    <my-button @click="onMyButtonClicked" :greet="greetText" /><br>
+    <reset-button initialValue="Hello" v-model="greetText" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import MyButton from '../components/MyButton.vue';
+import ResetButton from '../components/ResetButton.vue';
 
 @Component({
   components: {
     MyButton,
+    ResetButton,
   },
 })
 export default class Home extends Vue {
