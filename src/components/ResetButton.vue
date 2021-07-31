9893fa32993276308b5b753ad16067e6fcbe7211
murata <murata.small.baseball@gmail.com>
2021-07-31 14:54:00 +0900

    子から親に値を渡す

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,31 +1,28 @@
 <template>
-  <button @click="onClick">Reset</button>
+    <button @click="onClick">Reset</button>
 </template>
 
 <script lang="ts">
-import {Component, Emit, Prop, Vue} from "vue-property-decorator";
+    import {Component, Emit, Prop, Vue} from "vue-property-decorator";
 
-@Component
-export default class ResetButton extends Vue {
-  @Prop({required: true}) // required: trueによって値を必須にする
-    public initialValue!: string; // 変数名: 型名;
+    @Component
+    export default class ResetButton extends Vue {
+        private initialValue!: string;
 
-  // モデルのバインドの為に必要
-  @Prop()
-  public value!: string;
+        @Prop()
+        public value!: string;
 
-  // モデルのバインドの為に必要
-  @Emit()
-  public input(value: string){
+        /** ライフサイクルフック */
+        public created(){
+            this.initialValue = this.value;
+        }
 
-  }
+        @Emit()
+        public input(value: string) {
+        }
 
-  public onClick(){
-    this.input(this.initialValue);
-  }
-
-  public created(){
-    this.initialValue = this.value;
-}
-}
+        public onClick(){
+            this.input(this.initialValue);
+        }
+    }
 </script>
