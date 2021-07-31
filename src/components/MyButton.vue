31f25f421250f7bd9fe0093b1d2b18624c39acfe
murata <murata.small.baseball@gmail.com>
2021-07-31 13:19:22 +0900

    create my-button

---
+++ src/components/MyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>MyButton</button>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import { component } from "vue/types/umd";
+
+@Component
+export default class MyButton extends Vue{
+
+}
+</script>
